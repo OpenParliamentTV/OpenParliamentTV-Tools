288cfdb32f771cbbda40f236f812ac0ab0d90906
Olivier Aubert <contact@olivieraubert.net>
2022-09-28 09:25:59 +0200

    Do not try to handle older style data repo architecture

--- optv/parliaments/DE/update_and_merge.py
+++ optv/parliaments/DE/update_and_merge.py
@@ -108,18 +108,17 @@ if __name__ == "__main__":
         args.save_raw_data = True
         args.include_all_proceedings = True
     args.data_dir = Path(args.data_dir)
-    if (args.data_dir / "media").exists():
-        # Old-style directory layout (from OpenParliamentTV-Parsers)
-        args.media_dir = args.data_dir / "media"
-        args.proceedings_dir = args.data_dir / "proceedings"
-        args.output = args.data_dir / "merged"
-    else:
-        # New-style directory layout
-        args.media_dir = args.data_dir / "original" / "media"
-        args.proceedings_dir = args.data_dir / "original" / "proceedings"
-        args.output = args.data_dir / "processed"
+
     if args.cache_dir is None:
         args.cache_dir = args.data_dir / "cache"
     else:
         args.cache_dir = Path(args.cache_dir)
+
+    args.media_dir = args.data_dir / "original" / "media"
+    args.proceedings_dir = args.data_dir / "original" / "proceedings"
+    args.merged_dir = args.cache_dir / "merged"
+    args.aligned_dir = args.cache_dir / "aligned"
+    args.ner_dir = args.cache_dir / "ner"
+    args.processed_dir = args.data_dir / "processed"
+
     update_and_merge(args)
