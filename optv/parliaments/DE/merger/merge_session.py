ce17024dad7e02818f75b5be7edac1333069579c
Olivier Aubert <contact@olivieraubert.net>
2022-10-27 10:47:35 +0200

    Implement Needleman-Wunsch alignment

--- optv/parliaments/DE/merger/merge_session.py
+++ optv/parliaments/DE/merger/merge_session.py
@@ -21,7 +21,35 @@ def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
     return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
-def merge_item(proceeding, mediaitem):
+def merge_item(mediaitem, proceedingitems):
+    # We have both items - copy proceedings data into media item
+    # Make a copy of the media data
+    output = deepcopy(mediaitem)
+
+    first_proceeding = proceedingitems[0]
+    # Copy relevant data from proceedings
+    try:
+        output['agendaItem']['proceedingIndex'] = first_proceeding['agendaItem']['speechIndex']
+    except KeyError:
+        import pdb; pdb.set_trace()
+    output['agendaItem']['proceedingIndexes'] = [ p['agendaItem']['speechIndex'] for p in proceedingitems ]
+    output['agendaItem']['mediaIndex'] = mediaitem['agendaItem']['speechIndex']
+    # Merge people in case of multiple proceedings
+    # FIXME: we do a simple append for now, we should filter out duplicated elements, but preserve
+    # order
+    output['people'] = [ person
+                         for p in proceedingitems
+                         for person in p['people'] ]
+    # Merge textContents from all proceeedings
+    output['textContents'] = [ tc
+                               for p in proceedingitems
+                               for tc in p['textContents'] ]
+    output['documents'] = [ doc
+                            for p in proceedingitems
+                            for doc in p['documents'] ]
+    return output
+
+def merge_item_old(proceeding, mediaitem):
     # Non-matching case - return the unmodified value
     if proceeding is None:
         mediaitem['agendaItem']['proceedingIndex'] = None
@@ -241,19 +269,98 @@ def unmatched_count(proceedings_file, media_file, options=None):
         'unmatched_media_count': len(unmatched_media)
     }
 
+def needleman_wunsch_align(proceedings, media, options):
+    """Align data structures using Needleman-Wunsch algorithm
+    """
+    config = {
+        "speaker_weight": 4,
+        "title_weight": 2,
+        "merge_penalty": -1,
+        "split_penalty": -1,
+    };
+    def build_index(l):
+        return [
+            { "index": item['agendaItem']['speechIndex'],
+              "speaker": item['people'][0]['label'] if item['people'] else "NO_SPEAKER",
+              "title": item['agendaItem']['officialTitle'],
+            "item": item }
+            for item in l
+        ]
+    media_index = build_index(media)
+    proceedings_index = build_index(proceedings)
+
+    # Similarity score between 2 items
+    def similarity(m, p):
+        # FIXME: use string distance (Levensteihn?) rather than strict equality?
+        return config['speaker_weight'] * int(m['speaker'] == p['speaker']) + config['title_weight'] * int(m['title'] == p['title'])
+
+    # Build the [m, p] matrix with scores using the Needleman-Wunsch algorithm
+    # https://fr.wikipedia.org/wiki/Algorithme_de_Needleman-Wunsch
+    # Initialize a m x p matrix
+    scores = [ [ similarity(m, p) for p in proceedings_index ] for m in media_index ]
+    # Or 0-initialization?
+    # scores = [ [ 0 for p in proceedings_index ] for m in media_index ]
+
+    # Build the score matrix
+    for i in range(1, len(media_index)):
+        for j in range(1, len(proceedings_index)):
+            scores[i][j] = max( scores[i-1][j-1] + similarity(media_index[i], proceedings_index[j]),
+                                scores[i-1][j] + config['split_penalty'],
+                                scores[i][j-1] + config['merge_penalty'] )
+
+    # Now that the matrix is built, compute a path with a maximal score
+    path = []
+    i = len(media_index) - 1
+    j = len(proceedings_index) - 1
+    max_score = scores[i][j]
+    while i > 0 and j > 0:
+        path.append({ "media_index": i,
+                      "proceeding_index": j,
+                      "score": max_score,
+                      "media": media_index[i]['item'],
+                      "proceeding": proceedings_index[j]['item'],
+                     })
+        diagonal = scores[i - 1][j - 1];
+        up = scores[i][j - 1];
+        left = scores[i - 1][j];
+        if diagonal >= up and diagonal >= left:
+            i = i - 1
+            j = j - 1
+        elif left >= up:
+            i = i - 1
+        else:
+            j = j - 1
+    return path
+
 def merge_data(proceedings, media, options):
     """Merge data structures.
 
     If no match is found for a proceedings, we will dump the
     proceedings as-is.
     """
+    path = needleman_wunsch_align(proceedings, media, options)
+
+    # Group by media. There can be multiple proceedings
     return [
-        merge_item(p, m)
+        merge_item(group[0]['media'],
+                   [ i['proceeding'] for i in group ])
+        for group in [ list(group)
+                       for media_index, group in itertools.groupby(path, lambda i: i['media_index']) ]
+    ]
+
+def merge_data_old(proceedings, media, options):
+    """Merge data structures.
+
+    If no match is found for a proceedings, we will dump the
+    proceedings as-is.
+    """
+    return [
+        merge_item_old(p, m)
         for (p, m) in matching_items(proceedings, media, options)
     ]
 
 def matching_proceeding(mediafile: Path, proceedings_dir: Path) -> Optional[Path]:
-    p = proceedings_dir / mediafile.name.replace('media', 'data')
+    p = proceedings_dir / mediafile.name.replace('media', 'proceedings')
     if p.exists():
         return p
     else:
