ee3bd44b4d6ff25f1b67ef5b8e25ec3150ae055c
Olivier Aubert <contact@olivieraubert.net>
2022-11-03 19:52:21 +0100

    Cleanup update_and_merge behaviour
    Rename options and have behaviour more regular

--- optv/parliaments/DE/update_and_merge.py
+++ optv/parliaments/DE/update_and_merge.py
@@ -18,56 +18,69 @@ from merger.merge_session import merge_files_or_dirs
 from parsers.proceedings2json import parse_proceedings_directory
 
 def update_and_merge(args):
-    # Download/parse new media data
-    update_media_directory_period(args.from_period,
-                                  args.media_dir,
-                                  force=args.force,
-                                  save_raw_data=True,
-                                  retry_count=args.retry_count)
+    media_dir = args.data_dir / "original" / "media"
+    proceedings_dir = args.data_dir / "original" / "proceedings"
+    merged_dir = args.cache_dir / "merged"
+    aligned_dir = args.cache_dir / "aligned"
+    ner_dir = args.cache_dir / "ner"
+    processed_dir = args.data_dir / "processed"
 
-    # Download new proceedings data
-    download_plenary_protocols(args.proceedings_dir, False, args.from_period)
+    def publish_as_processed(sessionfile_list: list[str, Path]):
+        """Finalizing step - copy produced_files into processed
+        This will be called after each step that produced a correct (even
+    if incomplete) session file (merge, align, ner)
+        """
+        for session, path in sessionfile_list:
+            processed_file = processed_dir / f"{session}-session.json"
+            shutil.copyfile(path, processed_file)
 
-    # Update all proceedings that need to be updated
-    parse_proceedings_directory(args.proceedings_dir, args)
+    if args.download_original:
+        logger.info(f"Downloading media and proceeding data for period {args.period}")
+        # Download/parse new media data
+        update_media_directory_period(args.period,
+                                      media_dir,
+                                      force=args.force,
+                                      save_raw_data=True,
+                                      retry_count=args.retry_count)
+
+        # Download new proceedings data
+        download_plenary_protocols(proceedings_dir, fullscan=False, period=args.period)
+
+    # Update proceedings that need to be updated
+    parse_proceedings_directory(proceedings_dir, args)
 
     # Produce merged data
-    logger.info(f"Merging data from {args.media_dir} and {args.proceedings_dir} into {args.merged_dir}")
+    logger.info(f"Merging data from {media_dir} and {proceedings_dir} into {merged_dir}")
 
-    # Produce merged data into args.merged_dir
-    merged_files = merge_files_or_dirs(args.media_dir, args.proceedings_dir, args.merged_dir, args)
+    # Produce merged data into merged_dir
+    merged_files = merge_files_or_dirs(media_dir, proceedings_dir, merged_dir, args)
+    publish_as_processed(merged_files)
 
-    # We keep track of the last produced file for each concerned
-    # session. The keys can be updated in case of alignment/extraction.
-    # It is used in the final step of copying final data into processed
-    produced_files = dict(merged_files)
-
-    # Time-align produced files
+    # Time-align merged files
     if args.align_sentences:
-        if not args.aligned_dir.is_dir():
-            args.aligned_dir.mkdir(parents=True)
-        for session, sourcefile in merged_files:
-            filename = f"{session}-aligned.json"
-            aligned_file = args.aligned_dir / filename
-            # Since we use the output of merge_files_or_dirs we know
-            # that merged_files will be newer anyway, even if a
-            # previous alignmed file existed, so do not bother
-            # checking for timestamps
-            align_audiofile(sourcefile, aligned_file, args.lang, args.cache_dir)
-            produced_files[session] = aligned_file
+        logger.info("Updating time-alignment for merged files")
+        if not aligned_dir.is_dir():
+            aligned_dir.mkdir(parents=True)
+        for merged_file in merged_dir.glob('*-merged.json'):
+            session = merged_file.name[:5]
+            aligned_file = aligned_dir / f"{session}-aligned.json"
+            if (not aligned_file.exists() or
+                aligned_file.stat().st_mtime < merged_file.stat().st_mtime):
+                align_audiofile(merged_file, aligned_file, args.lang, args.cache_dir)
+                publish_as_processed([ session, aligned_file ])
 
-            if args.extract_entities:
-                ner_file = args.ner_dir / f"{session}-ner.json"
-                try:
-                    extract_entities_from_file(aligned_file, ner_file, args)
-                    produced_files[session] = ner_file
-                except FileNotFoundError:
-                    logger.error("Cannot extract entities from {ner_file} - File Not Found")
-
-    # Finalizing step. Copy all produced_files into processed
-    for session in sorted(produced_files.keys()):
-        processed_file = args.processed_dir / f"{session}-session.json"
-        shutil.copyfile(produced_files[session], processed_file)
+    # NER aligned files
+    if args.extract_entities:
+        logger.info("Updating NER for aligned files")
+        if not ner_dir.is_dir():
+            ner_dir.mkdir(parents=True)
+        for aligned_file in aligned_dir.glob('*-aligned.json'):
+            session = aligned_file.name[:5]
+            ner_file = ner_dir / f"{session}-ner.json"
+            if (not ner_file.exists() or
+                ner_file.stat().st_mtime < aligned_file.stat().st_mtime):
+                extract_entities_from_file(aligned_file, ner_file, args)
+                publish_as_processed([ session, ner_file ])
 
 if __name__ == "__main__":
 
@@ -77,8 +90,8 @@ if __name__ == "__main__":
     parser.add_argument("--debug", dest="debug", action="store_true",
                         default=False,
                         help="Display debug messages")
-    parser.add_argument("--from-period", type=int,
-                        help="Period to fetch (mandatory)")
+    parser.add_argument("--period", type=int,
+                        help="Period to fetch/consider (mandatory)")
     parser.add_argument("--retry-count", type=int,
                         dest="retry_count", default=0,
                         help="Max number of times to retry a media download")
@@ -86,18 +99,22 @@ if __name__ == "__main__":
                         default=False,
                         help="Force loading of data for a meeting even if the corresponding file already exists")
     parser.add_argument("--cache-dir", type=str, default=None,
-                        help="Cache directory")
+                        help="Cache directory (default is DATADIR/cache")
+    parser.add_argument("--lang", type=str, default="deu",
+                        help="Language")
+
+    parser.add_argument("--download-original", action=argparse.BooleanOptionalAction,
+                        default=True,
+                        help="Download original files")
     parser.add_argument("--align-sentences", action="store_true",
                         default=False,
                         help="Do the sentence alignment for downloaded sentences")
     parser.add_argument("--extract-entities", action="store_true",
                         default=False,
                         help="Do Entity extraction on aligned sessions (requires --align-sentences)")
-    parser.add_argument("--lang", type=str, default="deu",
-                        help="Language")
 
     args = parser.parse_args()
-    if args.data_dir is None or args.from_period is None:
+    if args.data_dir is None or args.period is None:
         parser.print_help()
         sys.exit(1)
     loglevel = logging.INFO
@@ -113,12 +130,5 @@ if __name__ == "__main__":
         args.cache_dir = args.data_dir / "cache"
     else:
         args.cache_dir = Path(args.cache_dir)
-
-    args.media_dir = args.data_dir / "original" / "media"
-    args.proceedings_dir = args.data_dir / "original" / "proceedings"
-    args.merged_dir = args.cache_dir / "merged"
-    args.aligned_dir = args.cache_dir / "aligned"
-    args.ner_dir = args.cache_dir / "ner"
-    args.processed_dir = args.data_dir / "processed"
 
     update_and_merge(args)
