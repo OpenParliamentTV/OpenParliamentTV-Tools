c7907e093c8b42490f1768c60e13fb7184d17953
Olivier Aubert <contact@olivieraubert.net>
2022-12-05 11:13:02 +0100

    Implement nel.py

---
+++ optv/parliaments/DE/nel/nel.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python3
+
+# Do Named Entity Linking for "structural" entities (people, factions)
+# in data files.
+
+# It can do "in-place" file enhancing (if you provide the same input
+# and output filenames)
+
+import logging
+logger = logging.getLogger(__name__)
+
+import argparse
+import json
+from pathlib import Path
+import sys
+
+# Allow relative imports if invoked as a script
+# From https://stackoverflow.com/a/65780624/2870028
+if __package__ is None:
+    module_dir = Path(__file__).resolve().parent
+    sys.path.insert(0, str(module_dir.parent))
+    __package__ = module_dir.name
+
+def link_entities(source: list, args, persons: dict, factions: dict) -> list:
+    """Link entities from source file
+    """
+    for speech in source:
+        for p in speech['people']:
+            label = p['label']
+            if persons.get(label):
+                # Found exact match
+                p['wid'] = persons[label]['id']
+                p['wtype'] = 'PERSON'
+            faction = p.get('faction')
+            if factions.get(faction):
+                f = factions[faction]
+                p['faction'] = {
+                    'wid': f['id'],
+                    'label': faction,
+                    'wtype': 'ORG'
+                }
+    return source
+
+def link_entities_from_file(source_file, output_file, persons, factions):
+    with open(source_file) as f:
+        source = json.load(f)
+
+    output = link_entities(source, args, persons, factions)
+
+    with open(output_file, 'w') as f:
+        json.dump(output, f, indent=2, ensure_ascii=False)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Link Named Entities from session file.")
+    parser.add_argument("source", type=str, nargs='?',
+                        help="Source JSON file")
+    parser.add_argument("output", type=str, nargs='?', default="-",
+                        help="Output file")
+    parser.add_argument("--person-data", action="store",
+                        default=None,
+                        help="Path to person.json file")
+    parser.add_argument("--faction-data", action="store",
+                        default=None,
+                        help="Path to faction.json file")
+    parser.add_argument("--debug", dest="debug", action="store_true",
+                        default=False,
+                        help="Display debug messages")
+    args = parser.parse_args()
+    if args.source is None:
+        parser.print_help()
+        sys.exit(1)
+    loglevel = logging.INFO
+    if args.debug:
+        loglevel = logging.DEBUG
+    logging.basicConfig(level=loglevel)
+
+    if not args.person_data and not args.faction_data:
+        # No data specified, nothing to do
+        logger.error("No reference data for persons or factions, bailing out.")
+        sys.exit(1)
+
+    persons = {}
+    factions = {}
+    if args.person_data:
+        with open(args.person_data) as f:
+            # Convert to a dict for basic lookup
+            persons = dict( (p['label'], p) for p in json.load(f) )
+    if args.faction_data:
+        with open(args.faction_data) as f:
+            factions = dict( (p.get('labelAlternative', p.get('label')), p) for p in json.load(f) )
+
+    link_entities_from_file(args.source, args.output, persons, factions)
